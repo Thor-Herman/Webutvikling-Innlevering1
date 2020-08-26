c33ba9fbbe64b4ad9b7b153ec19e0c65691cd6ee
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2020-08-26 20:20:51 +0200

    Make docs grid. Change display to instant

--- button.js
+++ button.js
@@ -1,9 +1,23 @@
 $(document).ready(() => {
     let infoRevClicked = 0;
     const infoRev = $(".info-rev");
+    const $info = $(".info");
     infoRev.click(() => {
         infoRevClicked = (infoRevClicked + 1) % 2;
-        $(".info").stop(true).slideToggle("slow").attr("display", "grid");
-        infoRevClicked ? infoRev.html("Hide Documentation") : infoRev.html("Show Documentation");
+        infoRevClicked ? showDocs() : hideDocs();
     });
+
+    const showDocs = () => {
+        infoRev.html("Hide Documentation");
+        $info.css({
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr",
+            gridTemplateRows: "repeat(3, 10em)"
+        })
+    }
+
+    const hideDocs = () => {
+        infoRev.html("Show Documentation")
+        $info.css("display", "none");
+    }
 });
