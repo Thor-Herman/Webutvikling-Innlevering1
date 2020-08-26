0be98a1b068b35972d6a3e76d77a20dcc0ccf025
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2020-08-26 22:43:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,51 +2,79 @@ $(document).ready(() => {
     const canvas = document.getElementById("canvas");
     const $canvas = $("#canvas");
     const context = canvas.getContext("2d");
-    let dpr = window.devicePixelRatio;
 
-    const createGradient = () => {
-        const gradient = context.createRadialGradient(75, 50, 5, 90, 60, 100);
-        gradient.addColorStop(0, "black");
-        gradient.addColorStop(0.5, "blue");
-        gradient.addColorStop(1, "red");
-        context.fillStyle = gradient;
-        context.fillRect(0, 0, 250, 200);
+    const renderBackGround = (angry) => {
+        context.fillStyle = angry ? '#FF0000' : '#4169e1';
+        context.fillRect(0, 0, 500, 300);
     };
 
-    const fixDpi = () => {
-        /*        const canv = $("canvas");
-                dpr = window.devicePixelRatio;
-                const height = +canv.css("height").slice(0, -2);
-                const width = +canv.css("width").slice(0, -2);
-                $canvas.attr("height", height * dpr).attr("width", width * dpr);*/
-    };
-
-    const renderBackGround = () => {
-        const canv = $("#canvas");
-        const width = canv.width();
-        const height = canv.height();
-        console.log(`width ${width} height ${height}`);
-        context.fillStyle = '#FF0000';
-        console.log(dpr);
-        context.fillRect(0, 0, width * dpr, height * dpr);
-    };
-
-    const renderCircle = () => {
+    const renderEyeball = (x, y) => {
         context.beginPath();
-        context.arc(50, 50, 50, 0, Math.PI * 2);
-        context.fillStyle = '#000000';
+        context.arc(x, y, 20, 0, Math.PI * 2);
+        context.fillStyle = '#FFFFFF';
+        context.fill();
+    };
+    const renderEye = (x, y) => {
+        context.moveTo(x, y);
+        context.ellipse(x, y, 45, 30, 0, 0, Math.PI*2);
+    }
+    const renderFace = () => {
+        context.beginPath();
+        context.fillStyle = "black";
+        context.fillRect(250, 100, 10, 50);
+        context.moveTo(250, 120);
+        context.lineTo(300, 150);
+        context.lineTo(250, 150);
+        renderEye(200, 70);
+        renderEye(300, 70);
+        context.fill();
+        renderEyeball(190, 63);
+        renderEyeball(310, 73);
+    }
+
+    const renderHappyFace = () => {
+        renderBackGround(false);
+        renderFace();
+        context.beginPath();
+        context.lineWidth = 3;
+        context.moveTo(160, 170);
+        context.lineTo(360, 170);
+        context.moveTo(160, 170);
+        context.bezierCurveTo(180, 270, 340, 270, 360, 170);
+        context.fillStyle = "white";
+        context.fill();
         context.stroke();
-    };
+    }
 
-    const renderCanvas = () => {
-        fixDpi();
-        renderBackGround();
-        renderCircle();
-    };
+    const renderEyeBrows = () => {
+        context.beginPath();
+        context.lineWidth = 6;
+        context.moveTo(240, 50);
+        context.lineTo(150, 10);
+        context.moveTo(260, 50);
+        context.lineTo(350, 10);
+        context.stroke();
+    }
 
-    renderCanvas();
+    const renderAngryFace = () => {
+        renderBackGround(true);
+        renderFace();
+        context.beginPath();
+        context.lineWidth = 3;
+        context.fillStyle = "white";
+        context.moveTo(160, 240);
+        context.lineTo(360, 240);
+        context.moveTo(160, 240);
+        context.bezierCurveTo(180, 140, 340, 140, 360, 240);
+        context.fill();
+        context.stroke();
+        renderEyeBrows();
+    }
 
-    $canvas.click(() => $(console.log("I got clicked")));
-    $canvas.mouseenter(() => $(console.log("Mouse enter"))).mouseleave(() => $(console.log("Mouse leave")));
-    //$(window).resize(() => renderCanvas());
+    renderAngryFace();
+
+    $canvas.mouseenter(() =>
+        $(renderHappyFace())).mouseleave(() =>
+            renderAngryFace()
+    )
 });
